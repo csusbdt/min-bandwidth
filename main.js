3a6739cab8ced5257d80738e7b605db24c50bb91
turner <csusbdt@gmail.com>
2013-01-22 22:30:09 -0800

    added database

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var http      = require('http');
 var router    = require('./router');
 var req_index = require('./req_index');
 var req_file  = require('./req_file');
+var db        = require('./model/db');
 
 // TODO: minify js and css as part of deployment process.
 // IDEA: minify at startup rather than as a build step.
@@ -19,10 +20,11 @@ process.env.PORT       = process.env.PORT.replace(' ', '');
 process.env.MONGO_PORT = process.env.MONGO_PORT.replace(' ', '');
 process.env.MONGO_HOST = process.env.MONGO_HOST.replace(' ', '');
 
-var n = 2;
+var n = 3;
 function onEnd() {
   if (--n === 0) router.start();
 }
 req_index.init(onEnd);
 req_file.init(onEnd);
+db.init(onEnd);
 
--- run.sh
+++
@@ -1,2 +0,0 @@
-PORT=5000 MONGO_PORT=1234 MONGO_HOST=localhost node main.js
-
