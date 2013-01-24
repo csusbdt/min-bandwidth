51a338d4c40eed9df885856050dae97c08d743f0
turner <csusbdt@gmail.com>
2013-01-24 01:51:45 -0800

    added cookie support

--- req_index.js
+++ req_index.js
@@ -1,22 +1,25 @@
-var url = require('url');
-var fs  = require('fs');
+var url    = require('url');
+var fs     = require('fs');
+var cookie = require('./app_cookie');
 
-var indexPage,
+var loginScreen,
+    titleScreen,
     appConfig = {
-      mongoPort: process.env.MONGO_PORT,
-      mongoHost: process.env.MONGO_HOST
+      mongoPort: process.env.MONGO_PORT
     };
 
 exports.init = function(cb) {
   fs.readFile('pages/index.html', 'utf8', function(err, file) {
     if (err) throw err;
-    indexPage = file.replace("APP_CONFIG", JSON.stringify(appConfig));
+    var indexPage = file.replace("CONFIG", JSON.stringify(appConfig));
+    loginScreen = indexPage.replace('SCREEN', 'index/login-0.html');
+    titleScreen = indexPage.replace('SCREEN', 'index/title-0.html');
     cb();
   });
 };
 
-// Use the following error function to report errors that may be 
-// generated in the request handler function.
+// Use the following error function to report errors that
+// may be generated in the request handler function.
 function error(req, res, err) {
   console.log(err.message);
   res.statusCode = 500;
@@ -24,10 +27,15 @@ function error(req, res, err) {
 }
 
 exports.handle = function(req, res) {
-  var appModel = {
-    date: new Date()
-  };
-  var body = new Buffer(indexPage.replace("APP_MODEL", JSON.stringify(appModel)), 'utf8');
+  var body, 
+      creds = cookie.creds(req),
+      model = { date: new Date() };
+  if (creds) {
+    model.creds = creds;
+    body = new Buffer(titleScreen.replace("MODEL", JSON.stringify(model)), 'utf8');
+  } else {
+    body = new Buffer(loginScreen.replace("MODEL", JSON.stringify(model)), 'utf8');
+  }
   res.writeHead(200, {
     'Content-Type'   : 'text/html',
     'Content-Length' : body.length,
